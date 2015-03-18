5fad38ada075f8c09eedca7f547fe8c601376524
HE Qichen <heqichen@gmail.com>
2015-03-19 00:26:02 +0800

    now can send half data

--- arduino/sensor/sensor.ino
+++ arduino/sensor/sensor.ino
@@ -5,10 +5,10 @@
 
 
 
-
 Mlx90620 mlx;
-
 double thermalArray[4][16];
+
+uint8_t sendBuffer[256];
 
 void setup()
 {
@@ -23,16 +23,37 @@ void loop()
 	mlx.updateFirArray();
 	mlx.getTo(thermalArray);
 
-	int i, j;
-	for (i=0; i<4; ++i)
+	int i, j, k;
+	sendBuffer[0] = 0x01;
+	for (i=0; i<2; ++i)
 	{
 		for (j=0; j<16; ++j)
 		{
-			Serial.print(thermalArray[i][j]);
-			Serial.print(" ");
+			uint8_t *doubleBuffer;
+			doubleBuffer = (uint8_t *)((void *)(thermalArray[i]+j));
+			for (k=0; k<4; ++k)
+			{
+				sendBuffer[1 + i*16*4 + j*4 + k] = doubleBuffer[k];
+			}
+			
 		}
-		Serial.println();
 	}
-	Serial.println();
-	delay(500);
+	emitData(sendBuffer, 2*16*4+1);
+
+	delay(1000);
+}
+
+void emitData(uint8_t *data, int length)
+{
+	Serial.write('S');
+	Serial.write('A');
+	uint8_t crc = 0x00;
+	int i;
+	for (i=0; i<length; ++i)
+	{
+		crc ^= data[i];
+		Serial.write(data[i]);
+	}
+	Serial.write(crc);
+	Serial.write('\n');
 }
