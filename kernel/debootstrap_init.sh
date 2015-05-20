5cea24f0bba3b60812313dd01d37859086bfa34d
HE Qichen <heqichen@gmail.com>
2015-05-21 01:24:38 +0800

    update init script for aptget

--- kernel/debootstrap_init.sh
+++ kernel/debootstrap_init.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 /debootstrap/debootstrap --second-stage
+
+echo "coobocEmu" > /etc/hostname
+
+cat <<END > /etc/apt/sources.list
+deb http://mirrors.sohu.com/ubuntu utopic main multiverse restricted universe
+END
